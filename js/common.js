e0df1f840d5dadab188db933a6a91b920a0e8ac1
KKM <rudans777@naver.com>
2023-09-27 09:59:12 +0900

    다섯번째업로드

--- js/common.js
+++ js/common.js
@@ -18,18 +18,25 @@ $(function(){
     
     
       $('header input[type="text"]').click(function () {
-        $('body').addClass('on');
+        // $('body').addClass('on');
         $('header .search_tab').addClass('on');
       });
     
-      $('body.on').click(function () {
-        $('header .search_tab').removeClass('on');
-        $('body').removeClass('on');
-      });
-    
+      // $('body.on').click(function () {
+      //   $('header .search_tab').removeClass('on');
+      //   $('body').removeClass('on');
+      // });
+
       $('header .search_close').click(function () {
         $('header .search_tab').removeClass('on');
-        $('body').removeClass('on');
+        // $('body').removeClass('on');
+      });
+
+      // body 클릭 시 search_tab 닫기
+      $(document).mouseup(function (e) {
+        if ($('header .search_tab').has(e.target).length === 0) {
+          $('header .search_tab').removeClass('on');
+        };
       });
     
       $('header .search_tab > ul li').click(function () {

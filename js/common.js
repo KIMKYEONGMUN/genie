2d79df8aa7941d91185a6068655f3040509d298c
KIMKYEONGMUN <rudans777@naver.com>
2023-10-14 23:30:27 +0900

    열세번째 업로드

--- js/common.js
+++ js/common.js
@@ -74,7 +74,9 @@ $(function(){
 
       $(window).resize(function () {
         if (window.outerWidth > 960) {
-          $('.search').stop().show();
+          $('.search').show();
+        } else {
+          $('.search').hide();
         };
       });
 
